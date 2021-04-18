d3d25d9ca35df27323a962de27abca99a80a9f40
moranat17 <68336978+moranat17@users.noreply.github.com>
2021-04-18 18:58:53 -0400

    Add files via upload

---
+++ Software Development/Programs/Program 2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Prog2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.datelabel = new System.Windows.Forms.Label();
+            this.lettertext = new System.Windows.Forms.TextBox();
+            this.dateoutputlabel = new System.Windows.Forms.Label();
+            this.timelabel = new System.Windows.Forms.Label();
+            this.timeoutputlabel = new System.Windows.Forms.Label();
+            this.hourstext = new System.Windows.Forms.TextBox();
+            this.welcomelabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter first letter of last name (uppercase):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter total credit hours:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "When Do I Register?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // datelabel
+            // 
+            this.datelabel.AutoSize = true;
+            this.datelabel.Location = new System.Drawing.Point(118, 229);
+            this.datelabel.Name = "datelabel";
+            this.datelabel.Size = new System.Drawing.Size(30, 13);
+            this.datelabel.TabIndex = 3;
+            this.datelabel.Text = "Date";
+            // 
+            // lettertext
+            // 
+            this.lettertext.Location = new System.Drawing.Point(267, 123);
+            this.lettertext.Name = "lettertext";
+            this.lettertext.Size = new System.Drawing.Size(100, 20);
+            this.lettertext.TabIndex = 4;
+            // 
+            // dateoutputlabel
+            // 
+            this.dateoutputlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dateoutputlabel.Location = new System.Drawing.Point(68, 259);
+            this.dateoutputlabel.Name = "dateoutputlabel";
+            this.dateoutputlabel.Size = new System.Drawing.Size(131, 23);
+            this.dateoutputlabel.TabIndex = 5;
+            // 
+            // timelabel
+            // 
+            this.timelabel.AutoSize = true;
+            this.timelabel.Location = new System.Drawing.Point(297, 229);
+            this.timelabel.Name = "timelabel";
+            this.timelabel.Size = new System.Drawing.Size(30, 13);
+            this.timelabel.TabIndex = 6;
+            this.timelabel.Text = "Time";
+            // 
+            // timeoutputlabel
+            // 
+            this.timeoutputlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeoutputlabel.Location = new System.Drawing.Point(267, 259);
+            this.timeoutputlabel.Name = "timeoutputlabel";
+            this.timeoutputlabel.Size = new System.Drawing.Size(100, 23);
+            this.timeoutputlabel.TabIndex = 7;
+            // 
+            // hourstext
+            // 
+            this.hourstext.Location = new System.Drawing.Point(267, 82);
+            this.hourstext.Name = "hourstext";
+            this.hourstext.Size = new System.Drawing.Size(100, 20);
+            this.hourstext.TabIndex = 8;
+            // 
+            // welcomelabel
+            // 
+            this.welcomelabel.AutoSize = true;
+            this.welcomelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.welcomelabel.Location = new System.Drawing.Point(118, 22);
+            this.welcomelabel.Name = "welcomelabel";
+            this.welcomelabel.Size = new System.Drawing.Size(227, 17);
+            this.welcomelabel.TabIndex = 9;
+            this.welcomelabel.Text = "Welcome to the Registration Portal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 316);
+            this.Controls.Add(this.welcomelabel);
+            this.Controls.Add(this.hourstext);
+            this.Controls.Add(this.timeoutputlabel);
+            this.Controls.Add(this.timelabel);
+            this.Controls.Add(this.dateoutputlabel);
+            this.Controls.Add(this.lettertext);
+            this.Controls.Add(this.datelabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label datelabel;
+        private System.Windows.Forms.TextBox lettertext;
+        private System.Windows.Forms.Label dateoutputlabel;
+        private System.Windows.Forms.Label timelabel;
+        private System.Windows.Forms.Label timeoutputlabel;
+        private System.Windows.Forms.TextBox hourstext;
+        private System.Windows.Forms.Label welcomelabel;
+    }
+}
+
---
+++ Software Development/Programs/Program 2/Form1.cs
@@ -0,0 +1,228 @@
+﻿//Grading ID: J7618//
+//Prog2//
+//Due Date: October 16 11:59 PM//
+//CIS 199-01//
+//This program helps us get familiar with nested if, if else and else statements, while applying it to real life//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace Prog2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string day = ""; //Creating a blank variable for the day to register//
+        string time = ""; //Creating a blank varaible for the time to register//
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Creating strings for each day that a student could schedule on, depending on their standing//
+            string senday = "Monday, Nov 4";
+            string junday = "Tuesday, Nov 5";
+            string sophdayone = "Wednesday, Nov 6";
+            string sophdaytwo = "Thursday, Nov 7";
+            string freshdayone = "Friday, Nov 8";
+            string freshdaytwo = "Monday, Nov 11";
+
+            const double SEN_HOURS = 90; //Creating a variable for the hours needed for senior status//
+            const double JUN_HOURS = 60; //Creating a variable for the hours needed for junior status//
+            const double SOPH_HOURS = 30; //Creating a variable for the hours needed for sophomore status//
+
+            char letter; //Creating a variable for the first letter in the student's last name//
+            
+
+            double hours; //Creating a variable for the hours a student has//
+
+            if (double.TryParse(hourstext.Text, out hours))
+            {
+                if (char.TryParse(lettertext.Text.ToUpper(), out letter))
+                {
+                    if (hours >= SEN_HOURS)
+                    {
+                        if (letter <= 'D')
+                        {
+                            day = senday;
+                            time = "11:30 AM";
+                        }
+                        else if (letter > 'D' && letter <= 'I')
+                        {
+                            day = senday;
+                            time = "2:00 PM";
+                        }
+                        else if (letter > 'I' && letter <= 'O')
+                        {
+                            day = senday;
+                            time = "4:00 PM";
+                        }
+                        else if (letter > 'O' && letter <= 'S')
+                        {
+                            day = senday;
+                            time = "8:30 AM";
+                        }
+                        else
+                        {
+                            day = senday;
+                            time = "10:00 AM";
+                        }
+               
+                    }
+                    else if (hours < SEN_HOURS && hours >= JUN_HOURS)
+                    {
+                        if (letter <= 'D')
+                        {
+                            day = junday;
+                            time = "11:30 AM";
+                        }
+                        else if (letter > 'D' && letter <= 'I')
+                        {
+                            day = junday;
+                            time = "2:00 PM";
+                        }
+                        else if (letter > 'I' && letter <= 'O')
+                        {
+                            day = junday;
+                            time = "4:00 PM";
+                        }
+                        else if (letter > 'O' && letter <= 'S')
+                        {
+                            day = junday;
+                            time = "8:30 AM";
+                        }
+                        else
+                        {
+                            day = junday;
+                            time = "10:00 AM";
+                        }
+                    }
+                    else if (hours < JUN_HOURS && hours >= SOPH_HOURS)
+                    {
+                        if (letter <= 'B')
+                        {
+                            day = sophdayone;
+                            time = "4:00 PM";
+                        }
+                        else if (letter > 'B' && letter <= 'D')
+                        {
+                            day = sophdaytwo;
+                            time = "8:30 AM";
+                        }
+                        else if (letter > 'D' && letter <= 'F')
+                        {
+                            day = sophdaytwo;
+                            time = "10:00 AM";
+                        }
+                        else if (letter > 'F' && letter <= 'I')
+                        {
+                            day = sophdaytwo;
+                            time = "11:30 AM";
+                        }
+                        else if (letter > 'I' && letter <= 'L')
+                        {
+                            day = sophdaytwo;
+                            time = "2:00 PM";
+                        }
+                        else if (letter > 'L' && letter <= 'O')
+                        {
+                            day = sophdaytwo;
+                            time = "4:00 PM";
+                        }
+                        else if (letter > 'O' && letter <= 'Q')
+                        {
+                            day = sophdayone;
+                            time = "8:30 AM";
+                        }
+                        else if (letter > 'Q' && letter <= 'S')
+                        {
+                            day = sophdayone;
+                            time = "10:00 AM";
+                        }
+                        else if (letter > 'S' && letter >= 'V')
+                        {
+                            day = sophdayone;
+                            time = "11:30 AM";
+                        }
+                        else
+                        {
+                            day = sophdayone;
+                            time = "2:00 AM";
+                        }
+                    }
+                    else
+                    {
+                        if (letter <= 'B')
+                        {
+                            day = freshdayone;
+                            time = "4:00 PM";
+                        }
+                        else if (letter > 'B' && letter <= 'D')
+                        {
+                            day = freshdaytwo;
+                            time = "8:30 AM";
+                        }
+                        else if (letter > 'D' && letter <= 'F')
+                        {
+                            day = freshdaytwo;
+                            time = "10:00 AM";
+                        }
+                        else if (letter > 'F' && letter <= 'I')
+                        {
+                            day = freshdaytwo;
+                            time = "11:30 AM";
+                        }
+                        else if (letter > 'I' && letter <= 'L')
+                        {
+                            day = freshdaytwo;
+                            time = "2:00 PM";
+                        }
+                        else if (letter > 'L' && letter <= 'O')
+                        {
+                            day = freshdaytwo;
+                            time = "4:00 PM";
+                        }
+                        else if (letter > 'O' && letter <= 'Q')
+                        {
+                            day = freshdayone;
+                            time = "8:30 AM";
+                        }
+                        else if (letter > 'Q' && letter <= 'S')
+                        {
+                            day = freshdayone;
+                            time = "10:00 AM";
+                        }
+                        else if (letter > 'S' && letter >= 'V')
+                        {
+                            day = freshdayone;
+                            time = "11:30 AM";
+                        }
+                        else
+                        {
+                            day = freshdayone;
+                            time = "2:00 AM";
+                        }
+                    }
+                }
+            }
+
+            //Displaying the day the student can register//
+            dateoutputlabel.Text = $"{day}";
+
+            //Displaying the time the student can register//
+            timeoutputlabel.Text = $"{time}";
+
+
+        }
+    }
+}
