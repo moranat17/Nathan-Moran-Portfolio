acfa7e2e34670e704cb2051d5313687a9cd04179
moranat17 <68336978+moranat17@users.noreply.github.com>
2021-04-18 19:00:11 -0400

    Add files via upload

---
+++ Software Development/Programs/Program 4/Program.cs
@@ -0,0 +1,86 @@
+﻿// Grading ID: J7618
+// Program 4
+// CIS 199-01
+// Due: 12/03/2019
+// This program is meant to utilize get;set; and as well as additional classes.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Prog4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //library book 1 object
+            LibraryBook libraryBook1 = new LibraryBook("Moby Dick", "Herman Melville", "Richard Bentley", 1851, "J987.6543", false);
+
+            //library book 2 object
+            LibraryBook libraryBook2 = new LibraryBook("Catching Fire", "Suzanne Collins", "Scholastic Corporation", 2009, "J012.3249", false);
+
+            //library book 3 object
+            LibraryBook libraryBook3 = new LibraryBook("The City of Ember", "Jeanne DuPrau", "Yearling", 2003, "J9999.888", false);
+
+            //library book 4 object
+            LibraryBook libraryBook4 = new LibraryBook("Lord of the Flies", "William Golding", "Faber and Faber", 1954, "J7394.5937", false);
+
+            //library book 5 object
+            LibraryBook libraryBook5 = new LibraryBook("Fahrenheit 451", "Ray Bradbury", "Simon & Schuster", 1953, "J4759.7459", false);
+
+
+            //array with library book objects
+            object[] bookArray = { libraryBook1, libraryBook2, libraryBook3, libraryBook4, libraryBook5 };
+
+            //for loop that prints objects contents on the console
+            for (int x = 0; x < bookArray.Length; x++)
+            {
+                WriteLine($"Book {x + 1} information:");
+                Write(bookArray[x].ToString());
+                WriteLine("");
+            }
+
+            WriteLine();
+            WriteLine();
+            WriteLine();
+
+            //changes made to 
+            libraryBook1.CheckOut();
+            libraryBook2.Publisher = "Nathan Moran";
+            libraryBook3.CallNumber = "J123.4567";
+            libraryBook4.CheckOut();
+            libraryBook5.Publisher = "Moran Books";
+
+
+            //for loop that prints objects contents on the console
+            for (int x = 0; x < bookArray.Length; x++)
+            {
+                WriteLine($"Book {x + 1} information after books have been checked out and changes to Publisher/Call number have been made:");
+                Write(bookArray[x].ToString());
+                WriteLine("");
+
+            }
+
+            WriteLine();
+            WriteLine();
+            WriteLine();
+
+
+            libraryBook1.ReturnToShelf();
+            libraryBook4.ReturnToShelf();
+
+
+            //for loop that prints objects contents on the console
+            for (int x = 0; x < bookArray.Length; x++)
+            {
+                WriteLine($"Book {x + 1} information after books have been returned:"); ;
+                Write(bookArray[x].ToString());
+                WriteLine("");
+
+            }
+        }
+    }
+}
---
+++ Software Development/Programs/Program 4/libraryBook.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class LibraryBook
+    {
+        //Boolean field for the checkout status
+        public bool checkoutStatus = false;
+
+        //precondition get: nothing
+        //postcondition get: Returns value for title
+        //precondition set: nothing
+        //postcondition set: value set for title
+        public string Title { get; set; }
+
+        //precondition get: nothing
+        //postcondition get: returns value for author
+        //precondition set: nothing
+        //postcondition set: value set for author
+        public string Author { get; set; }
+
+        //precondition get: nothing
+        //postcondition get: returns value for publisher
+        //precondition set: nothing
+        //postcondition set: value set for publisher
+        public string Publisher { get; set; }
+
+
+        private int copyrightYear;
+        public int CopyrightYear
+        {
+            //precondition: nothing
+            //postcondition: returns value of copyrightYear
+            get
+            {
+                return copyrightYear;
+            }
+            //precondition: tests if copyrightYear is greater than 0
+            //postcondtition: Sets copyrightYear = to value of copyrightYear or 2018 dependig on if it is greater than 0
+            set
+            {
+                if (copyrightYear >= 0)
+                    copyrightYear = value;
+                else
+                    copyrightYear = 2018;
+            }
+        }
+
+        //precondition get: nothing
+        //postcondition get: returns value for call number
+        //precondition set: nothing
+        //postcondition set: value set for call number
+        public string CallNumber { get; set; }
+
+        //precondition: accepts 5 parameters to construct object
+        //postocndition: object constructed with properties set above
+        public LibraryBook(string t, string a, string p, int cy, string cn, bool co)
+        {
+            Author = a;
+            Title = t;
+            Publisher = p;
+            CopyrightYear = cy;
+            CallNumber = cn;
+            co = IsCheckedOut();
+
+        }
+
+
+        //precondition: check out status is false
+        //postcondition: check out status changed to true
+        public void CheckOut()
+        {
+
+            checkoutStatus = true;
+
+        }
+
+        //precondition: check out status is true
+        //postcondition: check out status is returned to false because book has been returned
+        public void ReturnToShelf()
+        {
+            checkoutStatus = false;
+
+        }
+
+
+        //precondition: nothing
+        //postcondition: returns boolean value of check out status
+        public bool IsCheckedOut()
+        {
+            return checkoutStatus;
+        }
+
+
+        //precondition: ToString() method is in original form
+        //postcondition: ToString() method is overridden returning string with object's properties and check out status
+        public override string ToString()
+        {
+            return $"Title: {Title + Environment.NewLine}Author: {Author + Environment.NewLine}Publisher: {Publisher + Environment.NewLine}Copyright Year: {CopyrightYear + Environment.NewLine}Call Number: {CallNumber + Environment.NewLine}Check Out Status: {checkoutStatus + Environment.NewLine}";
+
+        }
+    }
+}
