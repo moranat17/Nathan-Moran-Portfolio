5b2022c3539b95c1b42b9c96501eff738dddc11c
moranat17 <68336978+moranat17@users.noreply.github.com>
2021-04-18 18:57:14 -0400

    Add files via upload

---
+++ Software Development/Practice/String Interpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5; //This is a dummy variable//
+            double y = 2.75; //This is the second dummy variable//
+            decimal z = 1.767879M; //This is a dummy decimal//
+
+            Console.WriteLine("{0} {1} {2}", x, y, z);
+            Console.WriteLine($"{x} {y:f1} {z}");
+        }
+    }
+}
