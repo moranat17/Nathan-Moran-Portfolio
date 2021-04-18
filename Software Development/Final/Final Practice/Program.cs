fcd8b528b51203762b0c83a250c5cb7914d396aa
moranat17 <68336978+moranat17@users.noreply.github.com>
2021-04-18 18:37:07 -0400

    Add files via upload

---
+++ Software Development/Final/Final Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] string1 = new string[100];
+
+            foreach (string x in string1)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
