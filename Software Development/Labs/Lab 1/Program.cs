b2fc5ddb1be456a21906b1ed9699f8dff64878c0
moranat17 <68336978+moranat17@users.noreply.github.com>
2021-04-18 18:50:36 -0400

    Add files via upload

---
+++ Software Development/Labs/Lab 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Grading ID: J7618//
+// Lab 1//
+//Due Date: September 1 11:59//
+//CIS 199-01//
+//Introduces students to C# and introduces themselves.//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Grading ID:       J7618");
+            WriteLine("Hobbies:          Watching Sports");
+            WriteLine("Favorite Book:    Modern Romance");
+            WriteLine("Favorite Movie:   Batman: The Dark Knight");
+        }
+    }
+}
